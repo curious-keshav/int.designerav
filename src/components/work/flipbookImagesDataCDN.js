6e16a995782bc2050c8e08240bb48873ef20d1f5
curious-keshav <keshavverma532@gmail.com>
2025-02-11 03:54:28 +0530

    added cloudify linked images

---
+++ src/components/work/flipbookImagesDataCDN.js
@@ -0,0 +1,60 @@
+import React from 'react'
+
+const flipbookImagesData = {
+    "projects": {
+      "project1": [
+        "1051_ctvju6",
+        "1050_waiood",
+        "1049_e3joy7",
+        "1043_r0gxki",
+        "1052_n4c5ve",
+        "1044_rnvzf3"
+      ],
+      "project3": [
+        "1005_epjrcn",
+        "1006_ootoso",
+        "1007_gkfwmx",
+        "1004_fky1qp",
+        "1003_mtkhe4"
+      ],
+      "project4": [
+        "1056_kj0s7r",
+        "1059_i1s75v",
+        "1058_htt12j",
+        "954_tb6vu4"
+      ],
+      "project5": [
+        "974_qwrfz7",
+        "973_cqu19j",
+        "988_qf2oge",
+        "997_r8ugxs",
+        "975_ojzbho",
+        "976_isvnoq",
+        "996_exlmue",
+        "979_sixkwv",
+        "989_fssudm",
+        "978_phtq5s",
+        "977_lok4kz"
+      ],
+      "project6": [
+        "1061_xdn3us",
+        "1062_oj4h8d",
+        "1063_o2mgfc"
+      ],
+      "project7": [
+        "1012_jdbsem",
+        "1013_acilu6"
+      ],
+      "project8": [
+        "1001_f0zmxo",
+        "999_jkry95",
+        "998_fq5cl7",
+        "1000_fepg10"
+      ]
+    }
+  }
+  
+
+export default flipbookImagesData
+
+
--- src/pages/work/[id].js
+++ src/pages/work/[id].js
@@ -1,6 +1,9 @@
 import FlipbookContainer from '@/components/work/FlipbookContainer';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
+import { CldImage } from 'next-cloudinary';
+import flipbookImagesData from '@/components/work/flipbookImagesDataCDN';
+import Link from 'next/link';
 
 const ProductPage = ({ product }) => {
   const router = useRouter();
@@ -10,31 +13,64 @@ const ProductPage = ({ product }) => {
 
   useEffect(() => {
     const updateScreenOrientation = () => {
-        const isPortrait = window.innerHeight > window.innerWidth;
-        setIsMobile(window.innerWidth <= 768);
-        setIsPotraitMode(isPortrait);
+      const isPortrait = window.innerHeight > window.innerWidth;
+      setIsMobile(window.innerWidth <= 768);
+      setIsPotraitMode(isPortrait);
     };
 
     updateScreenOrientation();
 
     window.addEventListener('resize', updateScreenOrientation);
     return () => {
-        window.removeEventListener('resize', updateScreenOrientation);
+      window.removeEventListener('resize', updateScreenOrientation);
     };
-}, []);
-
+  }, []);
+console.log(id,"keshav")
 
   return (
-    <div className='bg-[#1a1b1b]'>
-       <div className='col-span-12 '>
-            <div className='flex justify-center'>
-                <FlipbookContainer 
-                id={id}
-                isPortrait={isMobile || isPotraitMode}
-                isMobile={isMobile}
-                />
-            </div>
+    <div className='bg-[#1a1b1b] flex flex-col'>
+      <div className='col-span-12 '>
+        <div className='flex justify-center'>
+          <FlipbookContainer
+            id={id}
+            isPortrait={isMobile || isPotraitMode}
+            isMobile={isMobile}
+          />
         </div>
+
+        <div className="border border-gray-800 flex flex-col gap-6  m-8 bg-[#0D0D0D] !p-12 md:!p-4  md:!pt-8 h-fit rounded-2xl shadow-xl">
+          <h2 className="text-xl md:text-sm font-bold text-white text-center">
+            YOU MAY ALSO LIKE
+          </h2>
+
+          <div className="">
+            <div className="flex gap-3 md:gap-6 overflow-x-auto py-2 px-2 scrollbar-hide scroll-smooth snap-x">
+              {Object.entries(flipbookImagesData.projects).map(([projectId, images]) => (
+                <div key={projectId} className="snap-start shrink-0 w-[16rem] md:w-[8rem] transition-transform transform hover:scale-105">
+                  <Link className="relative rounded-xl overflow-hidden shadow-lg hover:shadow-2xl" href={`/work/${projectId[7]}`}>
+                    <CldImage
+                      src={images[0]}
+                      alt={`Project ${projectId}`}
+                      className="w-[15rem] h-[11rem] md:w-[8rem] md:h-[5rem] object-cover rounded-xl"
+                      priority
+                      sizes="(max-width: 768px) 100vw, (max-width:1200px) 50vw,33vw"
+                      width={350}
+                      height={220}
+                    />
+                    <div className="absolute inset-0 bg-black bg-opacity-40 hover:bg-opacity-20 transition-all duration-300"></div>
+                  </Link>
+                  <div className="mt-2 md:mt-3 text-center text-white font-medium mb-4 text-sm md:text-base">
+                    {projectId.toUpperCase()}
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+
+        </div>
+
+
+      </div>
     </div>
   );
 };
