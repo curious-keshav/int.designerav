3b336098ee8f2fdccd9fcac2bf14d50f1c54c587
curious-keshav <keshavverma532@gmail.com>
2025-02-12 01:50:47 +0530

    removed three js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,8 @@
 import React from 'react'
-import { StarsCanvas } from '../components/canvas'
 
 const Layout = ({ children, className = "" }) => {
   return (
     <div className={`w-full h-full inline-block z-0  p-16  px-32 dark:bg-dark ${className} xl:px-24 lg:px-16 md:px-12 sm:px-8  xl:p-12 lg:p-8 md:p-6 sm:p-4 `}>
-      {/* <StarsCanvas /> */}
       {children}
     </div>
   )
